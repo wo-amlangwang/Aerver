b5f3a413aefa6c3c251252cf5c1681db28a54649
wo-amlangwang <adamlangwang@gmail.com>
2015-07-09 09:56:48 -0400

    add messages

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var client = require('mongodb').MongoClient;
+var winston = require('winston');
 //var config = require ('./config/config.json');
 var url="mongodb://localhost:8080";
 var port="8900";
@@ -10,9 +11,18 @@ var port="8900";
 
 
 client.connect(url,function(err,db){
-  if(err)
-    console.log(err);
-  http.listen(port);
+  if(err){
+    winston.log('err',err);
+    process.exit();
+  }
+  winston.log('info','database connected');
+  http.listen(port, function(err){
+    if(err){
+      winston.log('err',err);
+      process.exit();
+    }
+    winston.log('info', 'server now listen on %d', port);
+  });
 
   io.on('connection',function(socket){
     require('./event.js')(socket,db,io);
