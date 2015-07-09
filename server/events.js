72ded4b2cd4f714a686498de1342b92a017c77d6
wo-amlangwang <adamlangwang@gmail.com>
2015-07-09 10:48:21 -0400

    add sign in event

--- server/events.js
+++ server/events.js
@@ -1,6 +1,8 @@
 module.exports=function(socket,db,io){
 
   var modules = require('./modules.js')(socket,db,io);
-  socket.on('hello',modules.sayhello);
-
+  socket.on('hello',modules.sayhello());
+  socket.on('getPostByName', modules.getPostByName(name));
+  socket.on('getPostByDate', modules.getPostByDate(date));
+  socket.on('signin',modules.signin(user));
 }
--- server/modules.js
+++ server/modules.js
@@ -1,7 +1,35 @@
+ph = require('password-hash');
+
 module.export=function(socket,db,io){
+  var users = db.collection('users');
+
+
+
+
   return{
     sayhello : function(){
       socket.emit('reply');
+    },
+    getPostByName : function(name){
+
+    },
+    getPostByDate : function(date){
+
+    }
+    signin : function(userinfo){
+      var userinfo = JSON.parse(userinfo);
+      if(! userinfo.hasOwnProperty('username')){
+        socket.emit('nameMissing');
+      }else if(! userinfo.hasOwnProperty('password')){
+        socket.emit('passwordMissing');
+      }else{
+        var hashedPassword = ph.generate(userinfo[username]+userinfo[password]);
+        delete userinfo['password'];
+        userinfo.push('hashedpassword' : hashedPassword);
+        users.insert(userinfo);
+        socket.emit('usercreated');
+      }
+
     }
   }
 }
